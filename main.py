a0102cbdedba3f15450f93a64588d7a67c256ddc
Nikolehunter123 <80729663+octane47@users.noreply.github.com>
2021-06-15 02:53:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#functions go here
+print("welcome to your annual maaori quiz within the following questions we will test your knowledge of the maaori language, goodluck and have fun remember its also a game so dont worry to much")
+#rules
+
+
+#main routine goes here
