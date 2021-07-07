ac9d2b18b9f7629522f2d905a187dad39093260e
Nikole Hunter <80729663+octane47@users.noreply.github.com>
2021-07-07 23:09:27 +0000

    beginning just welcoming the student to the quiz

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #functions go here
-print("welcome to your annual maaori quiz within the following questions we will test your knowledge of the maaori language, goodluck and have fun remember its also a game so dont worry to much")
+
 #rules
 
-
 #main routine goes here
+name = str(input("whats your name"))
+print("kia ora", name)
+print("welcome to your annual maaori quiz within the following questions we will test your knowledge of the maaori language, goodluck and have fun remember its also a game so dont worry to much ")
