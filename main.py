2ebbe08c6f8d3460b25cd31ade0051d157c87d52
Nikole Hunter <80729663+octane47@users.noreply.github.com>
2021-07-08 23:08:40 +0000

    I have finished designing the quiz and I have also figured out how I will input the questions and let the quizzer answer them and know the right and or wrong answer all there is to do know is get 10 questions and enable looping to restart the quiz at the end

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from testing1 import*
+
 
 
 #functions go here
@@ -17,11 +17,19 @@ print (" ")
 ##############################################
 ############### question 1 ###################
 ##############################################
+greeting= "QUESTION 1"
+sides = "!!!!" * 3
+greeting = "{} {} {}". format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+print (top_bottom)
+print (greeting)
+print (top_bottom)
 
-print ("QUESTION 1: how do you say hello in maaori")
+
+print ("how do you say hello in maaori")
 print ("A. kia ora ")
-print (" B. none ")
-print (" C. bunga")
+print ("B. aroha ")
+print ("C. kia ara")
 print ('')
 
 Q1answer = "A"
@@ -34,7 +42,69 @@ else:
     score = score + 1
 
 print ('Your current score is ' + str(score) + ' out of 10')
+print (" ")
+print (" ")
+
+##############################################
+############### question 2 ###################
+##############################################
+
+greeting= "QUESTION 2"
+sides = "!!!!" * 3
+greeting = "{} {} {}". format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+print (top_bottom)
+print (greeting)
+print (top_bottom)
+
+print ("how would you greet one person")
+print ("A. nau mai")
+print (" B. tena koutou")
+print (" C. tena koe")
+print ('')
+
+Q1answer = "c"
+Q1response= input('Your answer : ')
+
+if (Q1response != Q1answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q1response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10')
+print (" ")
+print (" ")
+##############################################
+############### question 3 ###################
+##############################################
 
 
+greeting= "QUESTION 3"
+sides = "!!!!" * 3
+greeting = "{} {} {}". format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+print (top_bottom)
+print (greeting)
+print (top_bottom)
+
+print ("aroha means....")
+print ("A. love")
+print (" B. tena koutou")
+print (" C. tena koe")
+print ('')
+
+Q1answer = "A"
+Q1response= input('Your answer : ')
+
+if (Q1response != Q1answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q1response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10')
+print (" ")
+print (" ")
 
 
