ccff7ef7579a8a88b5b7fc1e1dc1555a2be82101
Nikole Hunter <80729663+octane47@users.noreply.github.com>
2021-07-28 23:08:50 +0000

    adding another question and ben a while since I have saved and sent this to GitHub so just a new save and keep up to date type of thing

--- main.py
+++ main.py
@@ -5,11 +5,9 @@
 score = 0
 score =int(score)
 
-#rules
-
 #main routine goes here
-name = str(input("whats your name"))
-print("kia ora", name)
+name = str(input("whats your name "))
+print("kia ora ", name )
 print("welcome to your annual maaori quiz within the following questions we will test your knowledge of the maaori language, goodluck and have fun remember its also a game so dont worry to much ")
 print (" ")
 print (" ")
@@ -17,6 +15,8 @@ print (" ")
 ##############################################
 ############### question 1 ###################
 ##############################################
+
+#This code will decorate the title "question 1" allowing it to look nice and professional
 greeting= "QUESTION 1"
 sides = "!!!!" * 3
 greeting = "{} {} {}". format(sides, greeting, sides)
@@ -25,14 +25,14 @@ print (top_bottom)
 print (greeting)
 print (top_bottom)
 
-
+#asking the question
 print ("how do you say hello in maaori")
 print ("A. kia ora ")
 print ("B. aroha ")
 print ("C. kia ara")
 print ('')
-
-Q1answer = "A"
+#answering the question and checking it
+Q1answer = "a"
 Q1response= input('Your answer : ')
 
 if (Q1response != Q1answer):
@@ -59,8 +59,8 @@ print (top_bottom)
 
 print ("how would you greet one person")
 print ("A. nau mai")
-print (" B. tena koutou")
-print (" C. tena koe")
+print ("B. tena koutou")
+print ("C. tena koe")
 print ('')
 
 Q1answer = "c"
@@ -75,6 +75,8 @@ else:
 print ('Your current score is ' + str(score) + ' out of 10')
 print (" ")
 print (" ")
+
+
 ##############################################
 ############### question 3 ###################
 ##############################################
@@ -90,11 +92,44 @@ print (top_bottom)
 
 print ("aroha means....")
 print ("A. love")
-print (" B. tena koutou")
-print (" C. tena koe")
+print ("B. tena koutou")
+print ("C. tena koe")
 print ('')
 
-Q1answer = "A"
+Q1answer = "a"
+Q1response= input('Your answer : ')
+
+if (Q1response != Q1answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q1response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10')
+#empty prints are for spacing in the code so it doesn't look jumbled up
+print (" ")
+print (" ")
+
+##############################################
+############### question 4 ###################
+##############################################
+
+
+greeting= "QUESTION 4"
+sides = "!!!!" * 3
+greeting = "{} {} {}". format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+print (top_bottom)
+print (greeting)
+print (top_bottom)
+
+print (" i wanna go have some kai, what does kai mean?")
+print ("A. jacket")
+print ("B. food ")
+print ("C. sleep ")
+print ('')
+
+Q1answer = "b"
 Q1response= input('Your answer : ')
 
 if (Q1response != Q1answer):
@@ -106,5 +141,102 @@ else:
 print ('Your current score is ' + str(score) + ' out of 10')
 print (" ")
 print (" ")
+
+##############################################
+############### question 5 ###################
+##############################################
+
+
+greeting= "QUESTION 5"
+sides = "!!!!" * 3
+greeting = "{} {} {}". format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+print (top_bottom)
+print (greeting)
+print (top_bottom)
+
+print (" what part of the body does poku refer to")
+print ("A. arm ")
+print ("B. stomach")
+print ("C. ankle ")
+print ('')
+
+Q1answer = "b"
+Q1response= input('Your answer : ')
+
+if (Q1response != Q1answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q1response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10')
+print (" ")
+print (" ")
+
+##############################################
+############### question 6 ###################
+##############################################
+
+
+greeting= "QUESTION 6"
+sides = "!!!!" * 3
+greeting = "{} {} {}". format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+print (top_bottom)
+print (greeting)
+print (top_bottom)
+
+print (" what is a kiwi ")
+print ("A. a flightless bird that lives in new zealand ")
+print ("B. the name of a dolphin in the hamiltion zoo")
+print ("C. the maaori name for ankle")
+print ('')
+
+Q1answer = "a"
+Q1response= input('Your answer : ')
+
+if (Q1response != Q1answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q1response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10')
+print (" ")
+print (" ")
+
+##############################################
+############### question 7 ###################
+##############################################
+
+
+greeting= "QUESTION 7"
+sides = "!!!!" * 3
+greeting = "{} {} {}". format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+print (top_bottom)
+print (greeting)
+print (top_bottom)
+
+print (" i need a rakau pumahara. what is a rakau pumahara? ")
+print ("A. ")
+print ("B. ")
+print ("C. ")
+print ('')
+
+Q1answer = "a"
+Q1response= input('Your answer : ')
+
+if (Q1response != Q1answer):
+    print ('Sorry, that is incorrect!')
+else:
+    print ('Well done! ' + Q1response + ' is correct!')
+    score = score + 1
+
+print ('Your current score is ' + str(score) + ' out of 10')
+print (" ")
+print (" ")
+
 
 
